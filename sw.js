0e554bd0309ce6b9d612f2697a90e53cfa26df80
Cuasicoromoto <cuasicoromoto@gmail.com>
2026-03-25 23:28:33 -0400

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,10 @@
-const CACHE_NAME = 'catequesis-v0.1';
-const ASSETS = [
-  './gempro.html',
-  './manifest.json'
-];
+const CACHE_NAME = 'catequesis-cache-v0.2';
+const assets = ['./gempro.html', './manifest.json'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
