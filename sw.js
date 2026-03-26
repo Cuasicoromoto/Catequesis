b646e36325d3d4c262eaffc3e3e66acd7de0337a
Cuasicoromoto <cuasicoromoto@gmail.com>
2026-03-26 18:01:23 -0400

    Actualizar sw.js

---
+++ sw.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = 'catequesis-cache-v0.10';
+const urlsToCache =[
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+// Instalar el Service Worker y guardar los archivos en caché
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Archivos cacheados');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activar y limpiar cachés viejas (útil cuando actualices la app)
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Estrategia: "Network First" (Red primero, respaldo en caché)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        // Si hay internet, clonamos la respuesta y actualizamos la caché
+        if (response && response.status === 200) {
+          const responseClone = response.clone();
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, responseClone);
+          });
+        }
+        return response;
+      })
+      .catch(() => {
+        // Si no hay internet (falla el fetch), buscamos en la caché
+        return caches.match(event.request);
+      })
+  );
+});
